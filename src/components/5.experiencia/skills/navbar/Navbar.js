c10405b96a1b4a42d6759e93fecae2ce969890f6
Juan-calero <hello.juancalero@gmail.com>
2021-05-12 01:09:56 +0100

    scss cleanup II

--- src/components/5.experiencia/skills/navbar/Navbar.js
+++ src/components/5.experiencia/skills/navbar/Navbar.js
@@ -22,9 +22,9 @@ class Navbar extends React.Component {
                 onMouseEnter={cursorHoverBig}
                 onMouseLeave={cursorHoverSmall}
                 className="skills__navbar">
-                <span tabIndex="0" onMouseEnter={() => { this.handlerEnter('desen')}} onMouseLeave={() => this.handlerLeave('desen')} className="skills__navbar--p3">Desenvolvimento</span>
-                <span tabIndex="0" onMouseEnter={() => { this.handlerEnter('design')}} onMouseLeave={() => this.handlerLeave('design')} className="skills__navbar--p1">Design</span>
-                <span tabIndex="0" onMouseEnter={() => { this.handlerEnter('tools')}} onMouseLeave={() => this.handlerLeave('tools')} className="skills__navbar--p2">Ferramentas</span>
+                <span tabIndex="0" onMouseEnter={() => { this.handlerEnter('desen')}} onMouseLeave={() => this.handlerLeave('desen')} className="skills__navbar--p">Desenvolvimento</span>
+                <span tabIndex="0" onMouseEnter={() => { this.handlerEnter('design')}} onMouseLeave={() => this.handlerLeave('design')} className="skills__navbar--p">Design</span>
+                <span tabIndex="0" onMouseEnter={() => { this.handlerEnter('tools')}} onMouseLeave={() => this.handlerLeave('tools')} className="skills__navbar--p">Ferramentas</span>
             </div>
         )
     }
